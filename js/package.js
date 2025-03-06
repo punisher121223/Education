6a06e7dd364150e2e7e7dc83374a57d9de644fc2
punisher121223 <alirezaaz33221140@gmail.com>
2025-03-06 18:10:26 +0330

    Update package.js

--- js/package.js
+++ js/package.js
@@ -1,4 +1,10 @@
 function buyPackage(packageName, description, price) {
+    let currentUser = localStorage.getItem('currentUser');
+    if (currentUser === 'guest') {
+        alert('لطفاً ابتدا یک حساب کاربری بسازید!');
+        window.location.href = 'profile.html';
+        return;
+    }
     let message = `سلام، من می‌خوام بسته "${packageName}" با توضیحات "${description}" و قیمت ${price} تومان رو خریداری کنم.`;
     let telegramLink = `https://t.me/alireza_teacher?text=${encodeURIComponent(message)}`;
     window.open(telegramLink, '_blank');
